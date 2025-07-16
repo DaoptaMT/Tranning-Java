29b9a34a4b2417b3bead34961ea04110162db0f2
Truc Pham <kientruc582@gmail.com>
2025-07-16 16:11:07 +0700

    Fix: Check login when account is locked

--- src/main/java/com/mt/pharmacy_be/entity/UserEntity.java
+++ src/main/java/com/mt/pharmacy_be/entity/UserEntity.java
@@ -5,11 +5,11 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.Where;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
@@ -20,12 +20,6 @@ import java.util.UUID;
 @Where(clause = "flag_deleted = false")
 @Table(name = "app_user")
 @FieldDefaults(level = AccessLevel.PRIVATE)
-/**
- * Searhc.....
- * author: Thanh Truc
- * Date: 2023/10/20
- * description: Entity class representing a user in the application.
- */
 public class UserEntity extends BaseEntity implements UserDetails {
 
     @Id
@@ -46,28 +40,34 @@ public class UserEntity extends BaseEntity implements UserDetails {
     @OneToOne(mappedBy = "userEntity")
     CustomerEntity customerEntity;
 
+    @OneToMany(mappedBy = "userEntity")
+    private Set<UserRoleEntity> userRoleEntities = new HashSet<>();
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return userRoleEntities.stream()
+                .map(role -> new SimpleGrantedAuthority(
+                        role.getRoleEntity().getName().toString()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return flagOnline;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return flagOnline;
     }
 }
