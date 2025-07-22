24bff7bab53c807635044cc4dee3ab0a4e00e576
tinllsn <tinllsn1674@gmail.com>
2025-07-22 11:44:18 +0700

    hothix: fix create kind

--- src/main/java/com/mt/pharmacy_be/service/impl/KindOfMedicineService.java
+++ src/main/java/com/mt/pharmacy_be/service/impl/KindOfMedicineService.java
@@ -33,7 +33,7 @@ public class KindOfMedicineService implements KindOfMedicineServiceInterface {
 //        KindOfMedicineResponseDTO kind = getKindOfMedicineByCode(requestDTO.getCode());
         Optional<KindOfMedicine> kind = kindOfMedicineRepository.findByCode(requestDTO.getCode());
 
-        if (kind != null) {
+        if (kind.isPresent()) {
             throw new ApiException(ErrorCode.KIND_EXISTED);
         }
         KindOfMedicine kindOfMedicine = modelMapper.map(requestDTO, KindOfMedicine.class);
