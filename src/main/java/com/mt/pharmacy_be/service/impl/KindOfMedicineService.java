d31c6f7f390cd52d71a1650a4106ebc1089a4925
tinllsn <tinllsn1674@gmail.com>
2025-07-22 11:31:30 +0700

    hothix: fix template error response and add condition to create a new kind

--- src/main/java/com/mt/pharmacy_be/service/impl/KindOfMedicineService.java
+++ src/main/java/com/mt/pharmacy_be/service/impl/KindOfMedicineService.java
@@ -28,7 +28,12 @@ public class KindOfMedicineService implements KindOfMedicineServiceInterface {
 
     @Override
     public KindOfMedicineResponseDTO createKindOfMedicine(KindOfMedicineRequestDTO requestDTO) {
-        System.out.println("test");
+//        KindOfMedicineResponseDTO kind = getKindOfMedicineByCode(requestDTO.getCode());
+        Optional<KindOfMedicine> kind = kindOfMedicineRepository.findByCode(requestDTO.getCode());
+
+        if (kind != null) {
+            throw new ApiException(ErrorCode.KIND_EXISTED);
+        }
         KindOfMedicine kindOfMedicine = modelMapper.map(requestDTO, KindOfMedicine.class);
         kindOfMedicineRepository.save(kindOfMedicine);
         return modelMapper.map(getKindOfMedicineByCode(kindOfMedicine.getCode()),
